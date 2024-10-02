a6e163d4132e7578046af5a0e43c40167ebb7979
washington-shoji <shoji.washington@gmail.com>
2024-10-02 12:11:05 +1000

    - added a main entry point for process initialisation

---
+++ main.c
@@ -0,0 +1,8 @@
+// Import the dependency to be initialised
+#include "./core/hello-world.h"
+
+int main()
+{
+    // Initialise a processing program here i.e. the helloWorldInit()
+    helloWorldInit();
+}
